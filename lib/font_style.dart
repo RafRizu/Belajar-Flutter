7488fe4138d59a443c64d208ac2d573a2f0846ce
RafRizu <thisrafi10@gmail.com>
2023-11-08 09:37:19 +0700

    first commit

---
+++ lib/font_style.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const textSt1 = TextStyle(
+  fontSize: 26,
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  color: Colors.red,
+
+);
+const textSt2 = TextStyle(
+  fontSize: 14,
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w300,
+  color: Colors.black,
+
+);
+const navText = TextStyle(
+  fontSize: 24,
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+
+);
+const author = TextStyle(
+  fontSize: 12,
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w300,
+  color: Colors.black,
+
+);
